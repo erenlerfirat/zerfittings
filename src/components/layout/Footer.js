dc9760f589463357d86e0332af3ce032dc4538a7
erenlerfirat <erenler.firat@gmail.com>
2026-03-10 12:00:02 +0300

    Footer modified

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,3 +1,4 @@
+import tkn from '../../assets/tknlogo.png'  
 
 function Footer() {
     return (
@@ -6,10 +7,19 @@ function Footer() {
                 <div className="my-8 mx-4 p-4 border-t-2  md:w-full ">
                     <p className=""> <span className="font-semibold"> Zer Fittings</span>, vana tedariki, teknik servis, özel üretim, yedek parça ve global lojistik hizmetlerini tek çatı altında sunan uluslararası bir denizcilik çözümleri sağlayıcısıdır. Mühendislik uzmanlığı ve sahaya hazır servis ekipleriyle, dünya genelindeki armatörlere, işletmecilere ve tersanelere güvenilir, mevzuata uygun ve hızlı destek sunarak operasyonel sürekliliği ve uzun vadeli değer oluşturmayı garanti eder.</p>
                 </div>
-                <div className="my-8 mx-4 p-4 border-t-2 flex justify-center md:w-full ">
-                    <iframe className="w-96 h-72 lg:w-72 justify-center items-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d518.4527010838075!2d29.309765628299708!3d40.83347048905902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cadc317b8e1849%3A0x922b0b73a2ec059f!2sYayla%2C%20P%C4%B1nar%20Sk.%2C%2034944%20Tuzla%2F%C4%B0stanbul!5e0!3m2!1str!2str!4v1765282056086!5m2!1str!2str" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
-                    </iframe>
+                
+                <div className="my-8 mx-4 p-4 border-t-2 md:w-full flex flex-col items-center md:items-start text-center md:text-left">
+                    <img src={tkn} alt="TKN Logo" className="h-12 w-auto mb-4" />
+
+                    <ul className="p-0 space-y-1 text-sm">
+                        <li className="font-semibold">ZerFittings TKN ile iştiraktir</li>
+                        <li>Adres: Aydıntepe Mah. Sahil Bulvarı No:126/54 TUZLA/İSTANBUL</li>
+                        <li>Telefon: +90 545 334 35 35</li>
+                        <li>Email: <a href="mailto:tkn.denizcilik@gmail.com" className="hover:text-blue-300">tkn.denizcilik@gmail.com</a></li>
+                        <li>Ticaret Sicil No: 346775-5</li>
+                    </ul>
                 </div>
+
                 <div className="my-8 mx-4 p-4 border-t-2 md:w-full md:flex md:justify-center">
                      <ul className="p-0 space-y-2 text-sm list-disc">
                        <li><a href="#" className="hover:text-blue-500">Zer Fittings Hakkında</a></li>
