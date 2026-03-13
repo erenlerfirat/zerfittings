eb4805a1100657f3714a1ab7e24c1933a35e8e48
erenlerfirat <erenler.firat@gmail.com>
2026-03-14 00:24:28 +0300

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import Home from './components/pages/Home/Home.js';
+import { lazy, Suspense } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import About from './components/pages/About/About.js';
-import Contact from './components/pages/Contact/Contact.js';
-import StainlessClamp from './components/pages/Products/hygiene/StainlessClamp.js';
-import StainlessTee from './components/pages/Products/hygiene/StainlessTee.js';
-import StainlessFilter from './components/pages/Products/hygiene/StainlessFilter.js';
-import StainlessJoint from './components/pages/Products/hygiene/StainlessJoint.js';
-import WeldedFittings from './components/pages/Products/industry/WeldedFittings.js';
-import ThreadedFittings from './components/pages/Products/industry/ThreadedFittings.js';
-import Pipe from './components/pages/Products/pipe/Pipe.js';
-import Profile from './components/pages/Products/profile/Profile.js';
-import IndustrialCard from './components/pages/Home/ProductMenu/IndustrialCard.js';
-import StainlessHygieneCard from './components/pages/Home/ProductMenu/StainlessHygieneCard.js';
+
+const Home = lazy(() => import('./components/pages/Home/Home.js'));
+const About = lazy(() => import('./components/pages/About/About.js'));
+const Contact = lazy(() => import('./components/pages/Contact/Contact.js'));
+const StainlessClamp = lazy(() => import('./components/pages/Products/hygiene/StainlessClamp.js'));
+const StainlessTee = lazy(() => import('./components/pages/Products/hygiene/StainlessTee.js'));
+const StainlessFilter = lazy(() => import('./components/pages/Products/hygiene/StainlessFilter.js'));
+const StainlessJoint = lazy(() => import('./components/pages/Products/hygiene/StainlessJoint.js'));
+const WeldedFittings = lazy(() => import('./components/pages/Products/industry/WeldedFittings.js'));
+const ThreadedFittings = lazy(() => import('./components/pages/Products/industry/ThreadedFittings.js'));
+const Pipe = lazy(() => import('./components/pages/Products/pipe/Pipe.js'));
+const Profile = lazy(() => import('./components/pages/Products/profile/Profile.js'));
+const IndustrialCard = lazy(() => import('./components/pages/Home/ProductMenu/IndustrialCard.js'));
+const StainlessHygieneCard = lazy(() => import('./components/pages/Home/ProductMenu/StainlessHygieneCard.js'));
 
 function App() {
-
   return (
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="*" element={<Home />} />
         <Route index path="/Home" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/About" element={<About />} />
-        <Route path="/urunler/paslanmaz-dirsek" element={<StainlessJoint/>} />
-        <Route path="/urunler/paslanmaz-tee" element={<StainlessTee/>} />
-        <Route path="/urunler/paslanmaz-boru-kelepce" element={<StainlessClamp/>} />
-        <Route path="/urunler/paslanmaz-filtre" element={<StainlessFilter/>} />
-        <Route path="/urunler/disli-fittings" element={<ThreadedFittings/>} />
-        <Route path="/urunler/kaynakli-fittings" element={<WeldedFittings/>} />
-        <Route path="/boru" element={<Pipe/>} />
-        <Route path="/profil" element={<Profile/>} />
+        <Route path="/urunler/paslanmaz-dirsek" element={<StainlessJoint />} />
+        <Route path="/urunler/paslanmaz-tee" element={<StainlessTee />} />
+        <Route path="/urunler/paslanmaz-boru-kelepce" element={<StainlessClamp />} />
+        <Route path="/urunler/paslanmaz-filtre" element={<StainlessFilter />} />
+        <Route path="/urunler/disli-fittings" element={<ThreadedFittings />} />
+        <Route path="/urunler/kaynakli-fittings" element={<WeldedFittings />} />
+        <Route path="/boru" element={<Pipe />} />
+        <Route path="/profil" element={<Profile />} />
         <Route path="/industrial" element={<IndustrialCard />} />
         <Route path="/stainless" element={<StainlessHygieneCard />} />
       </Routes>
+    </Suspense>
   );
 }
 
